da7d3f3931c76a52d3b81050d1389f843ab0e78c
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2025-05-08 17:21:09 -0300

    change to orm query mode in data fetch and add fetch notebook materials

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,37 +1,43 @@
 import "server-only";
 
-import { and, eq } from "drizzle-orm";
+import { and, asc, eq } from "drizzle-orm";
 
 import { db } from "@/db";
-import { notebooksTable } from "@/db/schema";
+import { materialsTable, notebooksTable } from "@/db/schema";
+import { cache } from "react";
 import { verifySession } from "./server-utils";
-import { sleep } from "./utils";
 
-export async function fetchNotebooks() {
-  await sleep(3000);
+export const fetchNotebooks = cache(async () => {
   const session = await verifySession();
 
-  const notebooks = await db
-    .select()
-    .from(notebooksTable)
-    .where(eq(notebooksTable.userId, session.user.id))
-    .orderBy(notebooksTable.createdAt);
+  const notebooks = await db.query.notebooksTable.findMany({
+    where: eq(notebooksTable.userId, session.user.id),
+    orderBy: [asc(notebooksTable.createdAt)],
+  });
 
   return notebooks;
-}
+});
 
 export async function fetchNotebookById(notebookId: string) {
   const session = await verifySession();
 
-  const [notebook] = await db
-    .select()
-    .from(notebooksTable)
-    .where(
-      and(
-        eq(notebooksTable.userId, session.user.id),
-        eq(notebooksTable.id, notebookId),
-      ),
-    );
+  const notebook = await db.query.notebooksTable.findFirst({
+    where: and(
+      eq(notebooksTable.userId, session.user.id),
+      eq(notebooksTable.id, notebookId),
+    ),
+  });
 
   return notebook;
+}
+
+export async function fetchNotebookMaterials(notebookId: string) {
+  await verifySession();
+
+  const materials = await db.query.materialsTable.findMany({
+    where: and(eq(materialsTable.notebookId, notebookId)),
+    orderBy: [asc(materialsTable.createdAt)],
+  });
+
+  return materials;
 }
